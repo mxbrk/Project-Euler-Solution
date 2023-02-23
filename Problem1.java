bbf3125eb8b51861707b28cc3f2442863788446f
mxbrk <maximilian.bronkhorst@protonmail.com>
2023-02-24 00:05:30 +0100

    formatted code

--- Problem1.java
+++ Problem1.java
@@ -1,24 +1,23 @@
-class Problem1{
-  public static void main (String[]args){
+class Problem1 {
 
-  int a = 3;
-  int b = 5;
-  int n = 1;
-  int result = 1;
-  int count1 = 0;
-/*  for(n=1; n<10;n++){
-    result = a * n;
-    System.out.println(result);
-  }
-*/
-  do{
-  //  System.out.println(result);
-    result = a * n;
-    n++;
-    count1 =+ result;
-  }
-  while(result<10);
+  public static void main(String[] args) {
+    int a = 3;
+    int b = 5;
+    int n = 1;
+    int result = 1;
+    int count1 = 0;
+    /*     for (n = 1; n < 10; n++) {
+      result = a * n;
+      System.out.println(result);
+    }
+ */
+    do {
+      //  System.out.println(result);
+      result = a * n;
+      n++;
+      count1 = +result;
+    } while (result < 10);
 
-  System.out.println(count1);
+    System.out.println(count1);
   }
 }
--- Problem2.java
+++ Problem2.java
@@ -1,28 +1,24 @@
 //Fibonacci numbers < 4000000
-class Problem2{
-    public static void main(String[]args){
+class Problem2 {
 
-      int a = 0;
-      int b = 1;
-      int result = 0;
-      int sum = 0;
+  public static void main(String[] args) {
+    int a = 0;
+    int b = 1;
+    int result = 0;
+    int sum = 0;
 
-      for(int i = 1; i <= 4000000; i++)
-      {
-        if(result <= 4000000)
-        {
-          result = a + b;
-          a = b;
-          b = result;
-  //        System.out.println(result);
-        }
-            if(((result % 2) == 0) )
-            {
-              sum = sum + result;
-            }
+    for (int i = 1; i <= 4000000; i++) {
+      if (result <= 4000000) {
+        result = a + b;
+        a = b;
+        b = result;
+        //        System.out.println(result);
       }
-
-      System.out.println(sum);
-
+      if (((result % 2) == 0)) {
+        sum = sum + result;
+      }
     }
+
+    System.out.println(sum);
+  }
 }
