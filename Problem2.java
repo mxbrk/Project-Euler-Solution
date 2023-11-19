a60155201b308edb618d96faf6bd8552aa3d6870
mxbrk <98916337+mxbrk@users.noreply.github.com>
2023-11-19 13:23:22 +0100

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,9 +1,6 @@
 public class Problem2 {
   public static void main(String[] args) {
-    int a = 0;
-    int b = 1;
-    int result = 0;
-    int sum = 0;
+    int a=0, b=1, result=0, sum=0;
 
     for (int i = 1; i <= 4000000; i++) {
       if (result <= 4000000) {
